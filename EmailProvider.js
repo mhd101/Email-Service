e3de657aff47544fb496dbd2876854a3c62857ce
mhd101 <itzmohammadkhan@gmail.com>
2025-07-10 11:24:54 +0000

    Created Email Provider Interface

---
+++ EmailProvider.js
@@ -0,0 +1,9 @@
+// Act as an Interface for multiple email providers
+class EmailProvider {
+    async sendEmail(to, subject, body){
+        throw new Error("sendEmail() must be implemented");
+    }
+
+}
+
+module.exports = EmailProvider;
